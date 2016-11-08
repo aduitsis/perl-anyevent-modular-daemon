83b45f38162cea813eab58230254f872e182f4fe
Athanasios Douitsis <aduitsis@cpan.org>
2016-11-08 17:41:56 +0200

    Complete load records with metadata
    Load module now sends a complete record with a system name and
    metadata fields that can be used by logstash.

--- lib/Daemon/Plugin/LoadReport.pm
+++ lib/Daemon/Plugin/LoadReport.pm
@@ -8,6 +8,8 @@ use Moose;
 use Log::Log4perl;
 use BSD::getloadavg;
 
+use Sys::Hostname;
+
 with 'Daemon::Plugin';
 
 my $logger = Log::Log4perl::get_logger();
@@ -24,6 +26,25 @@ has period => (
         default => 30,
 );
 
+has type => (
+	is	=> 'ro',
+	isa	=> 'Str',
+	default	=> 'load',
+);
+
+has index => (
+	is	=> 'ro',
+	isa	=> 'Str',
+	default	=> 'omnidisco',
+);
+
+has host_name => (
+	is	=> 'ro',
+	isa	=> 'Str',
+	default	=> sub { hostname },
+);
+
+
 sub BUILD {
 	my $self = shift // die 'incorrect call';
 	$self->debug("creating periodic load reporter with period ".$self->period);
@@ -32,7 +53,17 @@ sub BUILD {
 		cb	=> sub {
 			my @loadavg = getloadavg();
 			$logger->trace("load average is ".join(',',@loadavg));
-			$self->send_to_next({ '1' => $loadavg[0] , '5' => $loadavg[1] , '15' => $loadavg[2] });
+			$self->send_to_next({
+				'@metadata' => {
+					time	=> time,
+					type	=> $self->type,
+					index	=> $self->index,
+				},
+				one	=> $loadavg[0],
+				five	=> $loadavg[1],
+				fifteen => $loadavg[2],
+				system	=> $self->host_name,
+			});
 		},
 	));
 }
